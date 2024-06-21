5bd01af1e0e7a68f77d836ecc5e70712968cff06
leonwolv <leonwolvekamp@gmail.com>
2024-06-21 16:33:02 +0200

    Update mailbox_aanmaken.ps1

--- mailbox_aanmaken.ps1
+++ mailbox_aanmaken.ps1
@@ -1,6 +1,6 @@
 $mailboxname = ""
 $mailaddres = $mailboxname + "domain"
-$beheerder = ""
+$Owner = = ""
 $Securitygroupname = $mailboxname + "_autorisatie" 
 $Securitygroupmail= $Securitygroupname + "domain"
 
@@ -13,3 +13,6 @@ New-Mailbox -Shared -name $mailboxname -PrimarySmtpAddress $mailaddres
 Add-MailboxPermission -Identity $mailaddres -User $Securitygroupmail -AccessRights FullAccess
 Add-RecipientPermission -Identity $mailaddres -AccessRights SendAs -Trustee $Securitygroupmail
 get-MailboxPermission -Identity $mailaddres
+Set-DistributionGroup -Identity $Securitygroupname -ManagedBy @{Add=$Owner}
+	
+Add-DistributionGroupMember -Identity $Securitygroupname -Member $Owner
